bf3b8b567700596a4ab04adeb4d2b60a3a9916af
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-17 21:25:38 +0100

    FEAT aggiunto un minimo di stile

--- testo.php
+++ testo.php
@@ -1,13 +1,13 @@
 <?php 
 $text = $_GET['text'];
 var_dump($_GET);
-echo 'Il testo inserito è ' . $text;
+echo '<h1>Il testo inserito è <em>' . $text . '</em> </h1>';
 ?>
 
 <?php 
 echo '<br>';
 $p2 = str_replace('ciao', '***', $text);
 
-echo 'Il testo censurato è '. $p2;
+echo '<h2> Il testo censurato è '. $p2 .'</h2>';
 
 ?>

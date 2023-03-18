2b134e53c8233e7fc38ba562b084e212b9468b50
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-18 21:05:37 +0100

    FEAT aggiunto strtolower

--- testo.php
+++ testo.php
@@ -1,11 +1,12 @@
 <?php 
 $text = $_GET['text'];
 var_dump($_GET);
-echo '<h1>Il testo inserito è <em>' . $text . '</em> </h1>';
+echo '<h1> Il testo inserito è <em>' . $text . '</em> </h1>';
 ?>
 
 <?php 
 echo '<br>';
+$text = strtolower($text);
 $p2 = str_replace('ciao', '***', $text);
 
 echo '<h2> Il testo censurato è '. $p2 .'</h2>';

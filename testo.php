9b6054b25555f0456636591456f0ec935a376baa
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-18 21:08:48 +0100

    FEAT aggiunte parole

--- testo.php
+++ testo.php
@@ -8,6 +8,18 @@ echo '<h1> Il testo inserito è <em>' . $text . '</em> </h1>';
 echo '<br>';
 $text = strtolower($text);
 $p2 = str_replace('ciao', '***', $text);
+$p2 = str_replace('cane', '***', $p2);
+$p2 = str_replace('gatto', '***', $p2);
+$p2 = str_replace('pippo', '***', $p2);
+$p2 = str_replace('pluto', '***', $p2);
+$p2 = str_replace('paperino', '***', $p2);
+$p2 = str_replace('topolino', '***', $p2);
+$p2 = str_replace('minnie', '***', $p2);
+$p2 = str_replace('paperina', '***', $p2);
+$p2 = str_replace('paperoga', '***', $p2);
+$p2 = str_replace('paperone', '***', $p2);
+$p2 = str_replace('paperina', '***', $p2);
+$p2 = str_replace('giulia', '***', $p2);
 
 echo '<h2> Il testo censurato è '. $p2 .'</h2>';
 
